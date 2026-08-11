0a4a4fb809b5382ff06daa4926d47ddbee742fd3
rlajino7-eng <rlajino7@gmail.com>
2026-08-11 11:59:44 -0400

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 const app = express();
 
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(__dirname));
 
 // ---------------------------------------------------------
 // 1. CONFIGURACIÓN GOOGLE DRIVE API
